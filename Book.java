e7e58734aa45fe71116390ac11d19f81736e6dc8
Guilherme-Avellar <85587714+Guilherme-Avellar@users.noreply.github.com>
2023-05-16 12:21:18 -0300

    Add files via upload

--- Book.java
+++ Book.java
@@ -1,14 +1,47 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula16_05;
 
-/**
- *
- * @author cefet
- */
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 public class Book {
+    private String name;
+    private double price;
+    private int qty = 0;
+    //Author author1[] = new Author[50]; // em vez disso usa-se o arraylist
+    ArrayList<Author> authorV = new ArrayList<Author>();
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public ArrayList<Author> getAuthorV() {
+        return authorV;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
     
+    public String toString(){
+//        String aux = "";
+//        for(int i =0; i< authorV.size();i++){
+//            aux = aux + authorV.get(i);
+//        }
+        
+        System.out.println("Book[name=" + name + ", authors"+ Arrays.toString(authorV)+"Author[name]}]");
+    }
+
 }
