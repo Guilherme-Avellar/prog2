cba03592466420584b34c64ef348c4166304f0f9
Guilherme-Avellar <85587714+Guilherme-Avellar@users.noreply.github.com>
2023-05-16 12:19:04 -0300

    Add files via upload

---
+++ Author.java
@@ -0,0 +1,10 @@
+package aula16_05;
+
+public class Author {
+    String name, email;
+    char gender;
+
+
+
+    
+}
---
+++ aula15_05.java
@@ -0,0 +1,11 @@
+package aula16_05;
+
+
+
+public class aula15_05 {
+
+    public static void main(String[] args) {
+    
+    }
+    
+}
