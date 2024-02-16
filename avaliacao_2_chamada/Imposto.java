933b6681cbde7df101d706f057f7da179a069ef8
Guilherme-Avellar <85587714+Guilherme-Avellar@users.noreply.github.com>
2024-02-16 11:52:22 -0300

    Delete avaliacao_2_chamada directory duplicate

--- avaliacao_2_chamada/Imposto.java
+++
@@ -1,9 +0,0 @@
-package guilhermecarneiro_av;
-
-public interface Imposto {
-    //para possiveis polimorfismos envolvendo imposto
-    //imposto, muito provavelmente, pode ser considerado um contrato
-    //Calculos de impostos podem mudar dependendo da situacao
-    public double calcularImposto();
-    
-}
